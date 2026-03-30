d6d416e44218a03513acab4f39355b41360c1960
vasko1300 <miners.sheds5r@icloud.com>
2026-03-30 22:22:45 +0800

    feat: add Hibernate implementation

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +16,107 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        String sql = """
+            CREATE TABLE if not exists users (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                name varchar(50),
+                last_name varchar(50),
+                age INT)""";
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Error while creating users table", ex);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS users";
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Failed to drop users table", ex);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.persist(new User(name, lastName, age));
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Failed to save user", ex);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+//        String hql = "delete from User where id = :id";
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+//            session.createQuery(hql)
+//                    .setParameter("id", id)
+//                    .executeUpdate();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Failed to drop users table", ex);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<User>();
+        String hql = "from User";
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            users = session.createQuery(hql, User.class).getResultList();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Failed to get users", ex);
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String hql = "delete from User";
+        Transaction tx = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.createQuery(hql).executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Failed to get users", ex);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,6 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import java.util.Properties;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.service.ServiceRegistry;
+
 public class Util {
     private static String url = "jdbc:mysql://localhost:3306/preproject113";
     private static String user = "root";
@@ -11,5 +19,45 @@ public class Util {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
+    }
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) { // Для многопоточного приложения нужна синхронизация или eager init
+            try {
+                Configuration configuration = getConfiguration();
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+//                Нужно перехватить в DAO
+                throw new RuntimeException("Failed to initialize Hibernate SessionFactory for DB URL: " + url, e);
+            }
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Properties settings = getProperties();
+
+        Configuration configuration = new Configuration();
+        configuration.setProperties(settings);
+        return configuration;
+    }
+
+    private static Properties getProperties() {
+        Properties settings = new Properties();
+        settings.put(AvailableSettings.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(AvailableSettings.URL, url);
+        settings.put(AvailableSettings.USER, user);
+        settings.put(AvailableSettings.PASS, password);
+        settings.put(AvailableSettings.SHOW_SQL, "true"); // Hibernate печатает SQL-запросы, которые отправляет в БД
+        settings.put(AvailableSettings.FORMAT_SQL, "true"); // Делает выводимые в консоль SQL-запросы более читаемыми
+//        settings.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQLDialect");
+//        settings.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+//        settings.put(AvailableSettings.HBM2DDL_AUTO, "create-drop");
+        return settings;
     }
 }
